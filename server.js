fadaf9350256b5fcc704ed2f7128c2ac61bcfeca
Djdayabi143 <jenad9914@gmail.com>
2025-07-02 21:26:54 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,5 @@
 
 
-const port = process.env.PORT || 3000;
-app.listen(port, () => console.log('Running on port', port));
-
 const express = require('express');
 const fetch = require('node-fetch');
 const FormData = require('form-data');
